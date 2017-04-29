5dcdd3ba61154d06dcb9145e1b70caab229cfbaa
marklangster <mark@freewiretech.com>
2017-04-29 15:03:25 -0700

    Fixed initial project and added google maps.

---
+++ Podfile
@@ -0,0 +1,5 @@
+source 'https://github.com/CocoaPods/Specs.git'
+target 'WindowSeat' do
+pod 'GoogleMaps'
+pod 'GooglePlaces'
+end
---
+++ WindowSeat/ViewController.swift
@@ -0,0 +1,27 @@
+
+import UIKit
+import GoogleMaps
+
+class ViewController: UIViewController {
+
+    override func loadView() {
+        
+        navigationItem.title = "Hello Map"
+    
+        
+        let camera = GMSCameraPosition.camera(withLatitude: 37.3984006,
+                                              longitude: -122.1461653,
+                                              zoom: 15)
+        let mapView = GMSMapView.map(withFrame: .zero, camera: camera)
+        
+        let marker = GMSMarker()
+        marker.position = camera.target
+        marker.snippet = "Hello World"
+        marker.appearAnimation = GMSMarkerAnimation.pop
+        marker.map = mapView
+        
+        view = mapView
+    }
+
+}
+
